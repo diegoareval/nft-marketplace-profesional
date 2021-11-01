3b1425a8806292e5798b768592ff0fe57731d5fa
Diego <diego2000avelar@gmail.com>
2021-10-31 19:32:20 -0600

    setup

---
+++ truffle-config.js
@@ -0,0 +1,39 @@
+
+module.exports = {
+  networks: {
+
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+    contract_directory: "./src/contracts",
+    contracts_build_directory: "./src/abis"
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }      }
+    }
+  },
+  // db: {
+    // enabled: false,
+    // host: "127.0.0.1",
+    // adapter: {
+    //   name: "sqlite",
+    //   settings: {
+    //     directory: ".db"
+    //   }
+    // }
+  // }
+};
