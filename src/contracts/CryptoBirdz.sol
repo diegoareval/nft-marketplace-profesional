945ee9b28c1a28bd72adf777dcae7ef559d7ea80
Diego <diego2000avelar@gmail.com>
2021-11-03 18:06:14 -0600

    testing minting functionality

--- src/contracts/CryptoBirdz.sol
+++ src/contracts/CryptoBirdz.sol
@@ -3,6 +3,19 @@ pragma solidity ^0.8.0;
 import './ERC721Connector.sol';
 
 contract CryptoBirdz is ERC721Connector {
+    // store nfts
+    string[] public kryptoBirds;
+
+    mapping(string => bool) _kryptoBirdsExist;
+
+    function mint(string memory _cryptoBird) public {
+        require(!_kryptoBirdsExist[_cryptoBird], 'KryptoBird already exist');
+         kryptoBirds.push(_cryptoBird);
+        uint _id = kryptoBirds.length -1;
+        _mint(msg.sender, _id);
+    _kryptoBirdsExist[_cryptoBird] = true;
+
+    }
 
     constructor() ERC721Connector('KriptoBirdz', 'KBIRDZ'){
     }
